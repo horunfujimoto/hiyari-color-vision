20de3fd88ddb7d8b3ac964439a439312f81f8f70
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-10-04 06:27:01 +0000

    [Add]gem追加

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,7 @@ class Member < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum is_active: { active: 0, banned: 1, inactive: 2 }
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+
+  has_one_attached :image #imageカラムが追記されたかのように扱うことができる
+  enum open_status: { open: 0, unopened: 1, full_disclosure: 2 }
+  enum level_status: { mild: 0, moderate: 1, severe: 2 }
+
 end
