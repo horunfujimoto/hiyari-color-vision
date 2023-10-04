ed289a74cad2ac972d04bd45b8373367a65aa7b2
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-10-04 15:33:11 +0000

    [Add]新規投稿ページ作成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -6,8 +6,21 @@ class Public::PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to posts_path #後で修正！
   end
 
   def edit
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:member_id, :tag_id, :title, :body, :place, :open_status, :level_status )
   end
 end
