bab7b3fe361bdb46014d5019b32b2d0a2a974077
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-10-08 14:18:30 +0000

    [Add]管理者側のコメント管理機能追加

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,12 @@
 class Admin::PostsController < ApplicationController
+
   def index
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def show
+    @post = Post.find(params[:id])
+    @comment = Comment.new
   end
+
 end
