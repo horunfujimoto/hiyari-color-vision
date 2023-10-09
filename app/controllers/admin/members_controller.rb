6b7ff863ed441d323d7e3aee08592e9a5a31f010
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-10-09 06:33:22 +0000

    [Add]マイページにいいね一覧タブ追加

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -6,13 +6,15 @@ class Admin::MembersController < ApplicationController
 
   def show
     @member = Member.find(params[:id])
+    @favorite_posts = Vision.includes(:post, :favorites)
+                      .where('favorites.member_id': @member.id)
+                      .order('favorites.created_at DESC')
+                      .page(params[:page])
+    # Vision モデルから Post モデルの情報を取得!これがないとvision内にpostのカラムがないとエラーが出る
+    @favorite_posts = @favorite_posts.map(&:post)
     @member_posts = Post.where(member_id: @member, open_status: [0, 2])
                       .order(created_at: "DESC")
                       .page(params[:page])
-    # @favorite_posts = Vision.includes(:post, :favorites)
-    #                   .where('favorites.member_id': @member.id)
-    #                   .order('favorites.created_at DESC')
-    #                   .page(params[:page])
     @unopened_posts = Post.where(member_id: @member, open_status: 1)
                       .order(created_at: "DESC")
                       .page(params[:page])
