f7215953bd18759edf27b60de6a6bfe21fefd5f2
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-10-16 21:44:41 +0000

    [Add]管理者topページ作成

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,6 +2,9 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
 
   def admin_top
+    @members_count = Member.count
+    @posts_count =  Post.count
+    @companies_count = Member.distinct.count(:company_password)
   end
 
 end
