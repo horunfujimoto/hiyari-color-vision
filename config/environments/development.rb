fb77e0006db3641a51086e0b0e4db7eb8fbe3fd0
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-12-16 08:26:13 +0000

    [Update]SSL接続を強制する記述追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  #SSL接続を強制
+  config.force_ssl = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  #SSL接続を強制
+  config.force_ssl = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
